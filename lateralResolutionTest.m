93f546d8a2f2db78b536471a62d7f57a32d1ea27
Justin Poon <justinjpoon@gmail.com>
2015-06-26 15:21:56 -0400

    Updated old axial/lateral resolution tests
    Updated to work with multiple baseline values.

--- lateralResolutionTest.m
+++ lateralResolutionTest.m
@@ -15,7 +15,7 @@ end
 % Get baseline value for this test
 for i = 1:size(baselineFile,1)
     if strcmp(baselineFile{i,1},'Lateral resolution')
-        baselineVal = baselineFile{i,2};
+        baselineVals = [baselineFile{i,2:7}];
     end
 end
 
@@ -24,28 +24,61 @@ labels = readLabels(imageFile);
 dist = [];
 for n = 1:numel(labels)
     % Check if label contains 'Dist'
-    ind = strfind(labels{1},'Dist');
+    ind = strfind(labels{n},'Dist');
     % If label contains 'Dist'
     if ~isempty(ind)
         % Find the first decimal number (the measurement) after 'Dist'
-        dist(n) = str2double(regexp(labels{1}(ind:end),'\d*\.\d*','match','once'));
+        dist(n) = str2double(regexp(labels{n}(ind:end),'\d*\.\d*','match','once'));
     end
 end
-newVal = sum(dist)/numel(dist);
+newVals = dist;
 
-disp(['Baseline value: ' num2str(baselineVal) ' mm']);
-disp(['New value: ' num2str(newVal) ' mm']);
+if numel(newVals)<4
+    view = 'sagittal';
+else
+    view = 'axial';
+end
+
+if strcmp(view,'axial')
+    % Display measurements for axial view
+    disp('Baseline values (axial plane):');
+    disp(['Proximal (left): ' sprintf('%.2f',baselineVals(1)) ' mm']);
+    disp(['Proximal (right): ' sprintf('%.2f',baselineVals(2)) ' mm']);
+    disp(['Distal (left): ' sprintf('%.2f',baselineVals(3)) ' mm']);
+    disp(['Distal (right): ' sprintf('%.2f',baselineVals(4)) ' mm']);
+    disp(' ');
+    disp('New values (axial plane):');
+    disp(['Proximal (left): ' sprintf('%.2f',newVals(1)) ' mm']);
+    disp(['Proximal (right): ' sprintf('%.2f',newVals(2)) ' mm']);
+    disp(['Distal (left): ' sprintf('%.2f',newVals(3)) ' mm']);
+    disp(['Distal (right): ' sprintf('%.2f',newVals(4)) ' mm']);
+    disp(' ');
+else
+    % Display measurements for sagittal view
+    disp('Baseline values (longitudinal plane):');
+    disp(['Proximal: ' sprintf('%.2f',baselineVals(5)) ' mm']);
+    disp(['Distal: ' sprintf('%.2f',baselineVals(6)) ' mm']);
+    disp(' ');
+    disp('New values (longitudinal plane):');
+    disp(['Proximal: ' sprintf('%.2f',newVals(1)) ' mm']);
+    disp(['Distal: ' sprintf('%.2f',newVals(2)) ' mm']);
+    disp(' ');
+end
 
 % Change in lateral resolution (in mm)
-change = abs(newVal - baselineVal);
-% Check if max depth has changed by more than 1 cm
-if change > 1
+if strcmp(view,'axial')
+    change = abs(newVals - baselineVals(1:4));
+else
+    change = abs(newVals - baselineVals(5:6));
+end
+% Get results (0 or 1 if fail or pass requirement)
+result = change<=1;
+% Check if lateral resolution has changed by more than 1 mm
+if any(change > 1)
     % Fail
-    result = 0;
     disp('Lateral resolution test: failed');
 else
     % Pass
-    result = 1;
     disp('Lateral resolution test: passed');
 end
 
