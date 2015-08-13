24c7010d37ce34f8491e278a32cfc50e070041b0
Justin Poon <justinjpoon@gmail.com>
2015-08-13 11:44:32 -0400

    Fixed volume 3D view graphics issues.
    - Set axes SortOrder property to 'depth' instead of 'childorder'

--- ProstateBrachyQA.m
+++ ProstateBrachyQA.m
@@ -598,22 +598,6 @@ function figure1_ResizeFcn(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Set all units to normalized
-set(findobj(hObject,'-property','Units'),'Units','normalized');
-
-% for str = {'volume','gridAlignment'}
-%     testName = str{1};
-%     % Get previous button position
-%     prevPos = get(handles.([testName '_button_prev']),'Position');
-%     % Get axes position
-%     axesPos = get(handles.([testName '_axes']),'Position');
-%     % Set new button positions to bottom center of axes
-%     newPrevPos = [0.5-prevPos(3), axesPos(2)+0.01, prevPos(3), prevPos(4)];
-%     newNextPos = [0.5, axesPos(2)+0.01, prevPos(3), prevPos(4)];
-%     set(handles.([testName '_button_prev']),'Position',newPrevPos);
-%     set(handles.([testName '_button_next']),'Position',newNextPos);
-% end
-
 
 % --- Executes during object creation, after setting all properties.
 function grayscale_table_CreateFcn(hObject, eventdata, handles)
